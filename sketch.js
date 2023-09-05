84e193a9fc6feeaae09f92f14a5548a6eb55a4ce
suzana79 <128698630+suzana79@users.noreply.github.com>
2023-09-05 12:05:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+ var nun1= 3;
+ var nun2= 7;
+ //console.log (nun1+nun2);
+
+ var soma;
+ var resultado;
+
+function calcularSoma(nun1, nun2){
+  soma= nun1 + nun2;
+  return soma;
+}
+ 
+ resultado = calcularSoma(nun1, nun2);
+ console.log('A soma de ', nun1, 'e', nun2, ' é ', resultado);
+
+ nun1= 17
+ nun2= 33
+
+ resultado = calcularSoma(nun1, nun2);
+ console.log('A soma é:' , resultado);
