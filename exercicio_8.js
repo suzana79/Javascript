9930597668a1c0a2359a34f060a36e04feb25fda
suzana79 <128698630+suzana79@users.noreply.github.com>
2023-11-07 11:31:28 -0300

    commit

--- exercicio_8.js
+++ exercicio_8.js
@@ -1,17 +1,33 @@
-var board = document.getElementById('board');
-var message = document.getElementById('message');
-
-let jogadorAtual = 'x';
-let cells =  Array(9).fill('');
-let gameover = false;
-
-for (i = 0;i < 9; i++) {
-    let cell = document.createElement('div');
-    cell.classList('cell');
-    cell.addEventListener('click', () => handleCellClick (i));
-    board.appendChild(cell);
+body {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    text-align: center;
+    background-color: rgb(144, 179, 243);
 }
 
-function handleCellClick(index){
-    
+.board {
+    display: grid;
+    grid-template-columns: repeat(3,100px);
+    gap: 10px;
+    margin: 30px auto;
+    width: 320px;
+}
+.cell {
+    width: 100px;
+    height: 100px;
+    font-size:24px;
+    text-align: center;
+    line-height: 100px;
+    background-color: black;
+    cursor: pointer;
+    border: 2px solid whitesmoke;
+}
+
+.cell:hover {
+    background-color: lightslategray;
+
+}
+
+.message {
+    font-size: 40px;
+    margin: 30px;
 }
