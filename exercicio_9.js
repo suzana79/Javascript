4ad8dcabe98efda77afa1c94b9946e7a25cdf2b4
suzana79 <128698630+suzana79@users.noreply.github.com>
2023-11-23 08:53:56 -0300

    Add files via upload

--- exercicio_9.js
+++ exercicio_9.js
@@ -2,13 +2,14 @@ const mensagensDivertidas =
 [
  "Sabe qual o remédio que vai na padaria por você? O Buscopan."
  "Socorro, o lensol tá mi matano! Vc está em mal lensois"
+ 
 ]
 
 let botaoDivertido = document.getElementById("botaoDivertido");
 let mensagemDivertida = document.getElementById("mensagemDivertida");
 
 botaoDivertido.addEventListener('click', function () {
-    //Math.floor(x) retornna o menor número inteiro dentre o número "x".
+    //Math.floor(x) retorna o menor número inteiro dentre o número "x".
     //Math.random() retorna um número pseudo-aleatório no intervalo
     const mensagemAleatoria = [Math.floor(Math.random() * mensagensDivertidas.length)]
     
